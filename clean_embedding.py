43c28de5d38772b875210ed9f7ae0e10b536acbc
Zhenduo Wang <30311623+JeromWang@users.noreply.github.com>
2017-09-24 12:02:35 -0500

    Add files via upload

---
+++ clean_embedding.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-  
+# file: FileMerage.py  
+#  
+  
+import os  
+import random
+import re
+
+mergefiledir = os.getcwd()+'\\'+'vectorized_data'
+filenames = os.listdir(mergefiledir)  
+
+if os.path.isdir(mergefiledir):
+	if not os.path.isdir(os.path.join(os.getcwd(), 'clean_data')):
+		os.mkdir(os.path.join(os.getcwd(), 'clean_data'))
+
+for filename in filenames:
+	output_path = os.getcwd()+'\\'+'clean_data'+'\\'+filename
+	output = open( output_path,'w')  
+	filepath = mergefiledir+'\\'+filename  
+	for line in open(filepath):  	
+		m = re.match('(.+).txt\$\$(.+)', line)
+		author = m.group(1)
+		output.write(author)
+		vectors = m.group(2)
+		vector_list = re.split(' ',vectors)
+		for i,v in enumerate(vector_list):
+			output.write(' '+str(i+1)+':'+v)
+		output.write('\n')				
+		
+	output.close()  
+
+
+
