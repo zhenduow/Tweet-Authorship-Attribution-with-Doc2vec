04c506eb19ff0c38800f2d2b1f4f6179e5ea942c
Zhenduo Wang <30311623+JeromWang@users.noreply.github.com>
2017-11-03 09:04:02 -0500

    Update infer_test.py

--- infer_test.py
+++ infer_test.py
@@ -1,31 +1,32 @@
-#python example to infer document vectors from trained doc2vec model
+# -*- coding:utf-8 -*-
 import gensim.models as g
 import codecs
 import os
 
+'''
+Load the saved doc2vec model and infer docvecs for the authorship attribution set.
+'''
 
 #inference hyper-parameters
 start_alpha=0.01
 infer_epoch=100
 
 #load model
-model="model_dmpv.bin"
+model="model.bin"
 m = g.Doc2Vec.load(model)
 
-
-#vectorize test set	
-mergefiledir = os.getcwd()+'\\'+'data'
+#make folders for the inference vecs	
+mergefiledir = os.getcwd()+'\\'+'ebd_data'
 if os.path.isdir(mergefiledir):
 	if not os.path.isdir(os.path.join(mergefiledir, 'embedding')):
 		os.mkdir(os.path.join(mergefiledir, 'embedding'))
 filenames = os.listdir(mergefiledir)  
 
-#parameters
+#infer test vectors
 for filename in filenames:  
 	test_docs = mergefiledir+'\\'+filename  
 	output_path = mergefiledir+'\\'+'embedding'+'\\'+filename
 	test_docs = [ x.strip().split() for x in codecs.open(test_docs, "r", "utf-8").readlines() ]
-	#infer test vectors
 	output = open(output_path, "w")
 	for d in test_docs:
 		output.write( filename +"$$" )
